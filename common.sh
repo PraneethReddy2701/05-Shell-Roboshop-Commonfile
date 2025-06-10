b6557a65a09011d6ba5305431c3330156258f45e
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-10 18:42:11 +0530

    mongodb

---
+++ common.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started running at : $(date)"
+
+check_root()
+{
+    if [ $USER_ID -ne 0 ]
+    then
+        echo -e "ERROR : $R Please run the script with root access $N" 
+        exit 1
+    else
+        echo "Running the script with root user" 
+    fi
+}
+
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+print_time()
+{
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+    echo -e "Script executed successfully. Time taken : $Y $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+}
