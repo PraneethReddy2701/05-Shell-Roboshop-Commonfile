ed3f2b4297016bd3f6fa2a4b6dbece7cbde67f70
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-10 18:26:46 +0530

    Common file and mongodb

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root_user
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+VALIDATE $? "Copying MongoDB Repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "Editing MongoDB conf file for remote conncections"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MONGODB"
+
+print_time
